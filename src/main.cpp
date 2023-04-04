621b3867bb667c060d6c809c71e3b468f4b8d76d
Sherlock Wan <tingzhou@ualberta.ca>
2023-04-04 19:11:04 +0100

    merges made

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <MQ135.h>
 #include "Secrets.h"
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
@@ -15,12 +16,22 @@
 
 #define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+// Add the necessary headers
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+const char *AP_SSID = "ESP32AP";
+const char *AP_PASSWORD = "123456";
+// Add a constant for the server port
+const uint16_t serverPort = 80;
 
+// Create a Wi-Fi server instance
+WiFiServer tcpServer(serverPort);
+MQ135 mq135(MQ135_PIN);
 Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
-int air_quality;
-int sound_level;
+float air_quality;
+float sound_level;
 
 static const char *SERVICE_UUID = "3a4e2ff2-c9fb-11ed-afa1-0242ac120002";
 static const char *CHARACTERISTIC_UUID = "7214fb32-c9fb-11ed-afa1-0242ac120002";
@@ -37,14 +48,14 @@ class CharacteristicCallbacks : public NimBLECharacteristicCallbacks
   }
 };
 
-int readAndDisplayAirQuality()
+float readAndDisplayAirQuality()
 {
 
-  int air_quality = analogRead(MQ135_PIN);
-  digitalWrite(BUZZER_PIN, HIGH);
+  
+  float air_quality = mq135.getPPM();
+  /* digitalWrite(BUZZER_PIN, HIGH);
   delay(1000);
-  digitalWrite(BUZZER_PIN, LOW);
-
+  digitalWrite(BUZZER_PIN, LOW); */
   matrix.clear();
   matrix.setCursor(0, 0);
   matrix.print(air_quality);
@@ -54,17 +65,32 @@ int readAndDisplayAirQuality()
   return air_quality;
 }
 
-int readAndDisplaySoundLevel()
+float readAndDisplaySoundLevel()
 {
 
-  int sound_level = analogRead(MAX_PIN);
+  float max9814_gain = 60.0;            // Set this to the gain value you have selected for the MAX9814 (40dB, 50dB, or 60dB)
+  float adc_ref_voltage = 3.3;          // Set this to your ADC reference voltage (e.g., 3.3V or 5V)
+  // float microphone_sensitivity = -44.0; // Set this to the sensitivity of your electret microphone (e.g., -44 dBV/Pa)
+
+  int mic_value = analogRead(MAX_PIN);
+  Serial.println(mic_value);
+  float output_voltage = (float)mic_value / 4095.0 * adc_ref_voltage;
+
+  // Calculate the input-referred voltage of the microphone
+  // float input_voltage = output_voltage / pow(10, max9814_gain / 20);
+  // float sensitivity_v_per_pa = pow(10, microphone_sensitivity / 20) * 0.001;
+  // float sound_pressure = input_voltage / sensitivity_v_per_pa;
+
+  // Calculate the sound pressure level in dB SPL
+  // Calculate the sound pressure level in dB SPL
+  float sound_level1 = 20 * log10(output_voltage / adc_ref_voltage);
 
   matrix.clear();
   matrix.setCursor(0, 0);
-  matrix.print(sound_level);
+  matrix.print(sound_level1);
   matrix.writeDisplay();
   delay(1000);
-  return sound_level;
+  return sound_level1;
 }
 
 String get_wifi_status(int status)
@@ -97,8 +123,8 @@ void messageHandler(char *topic, byte *payload, unsigned int length)
   deserializeJson(doc, payload);
   const char *message = doc["message"];
   Serial.println(message);
-  int air_quality = doc["air_quality"];
-  int soung_level = doc["sound_level"];
+  float air_quality = doc["air_quality"];
+  float soung_level = doc["sound_level"];
   for (int i = 0; i < length; i++) 
   {
     Serial.print((char)payload[i]); // Pring payload content
@@ -106,9 +132,11 @@ void messageHandler(char *topic, byte *payload, unsigned int length)
     char buzzer = (char)payload[62]; // Extracting the controlling command from the Payload to Controlling Buzzer from AWS
     Serial.print("Command: ");
     Serial.println(buzzer);
-    if (air_quality == '0' || sound_level == '0')
+    if (air_quality > 500 && sound_level > -5)
     {
       digitalWrite(BUZZER_PIN, HIGH);
+      delay(1000);
+      digitalWrite(BUZZER_PIN, LOW);
     }
     else
     {
@@ -118,7 +146,9 @@ void messageHandler(char *topic, byte *payload, unsigned int length)
 
 void connectAWS()
 {
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(AP_SSID, AP_PASSWORD);
+  IPAddress IP = WiFi.softAPIP();
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Connecting to Wi-Fi");
@@ -159,15 +189,24 @@ void connectAWS()
 
   Serial.println("AWS IoT Connected!");
 }
-void publishMessage()
-{
-  StaticJsonDocument<200> doc;
-  doc["air_quality"] = air_quality;
-  doc["sound_level"] = sound_level;
-  char jsonBuffer[200];
-  serializeJson(doc, jsonBuffer); // print to client
+void publishMessage(float air_quality, float sound_level, int result) {
+  if (!client.connected()) {
+    connectAWS();
+  }
 
-  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+  // Create a JSON formatted message to send
+  StaticJsonDocument<256> jsonDoc;
+  jsonDoc["air_quality"] = air_quality;
+  jsonDoc["sound_level"] = sound_level;
+  jsonDoc["result"] = result;
+
+  char messageBuffer[256];
+  serializeJson(jsonDoc, messageBuffer);
+  Serial.println("Sending message to AWS IoT:");
+  Serial.println(messageBuffer);
+
+  // Publish the message to AWS IoT
+  client.publish(AWS_IOT_PUBLISH_TOPIC, messageBuffer);
 }
 
 /* void processBluetoothData()
@@ -193,7 +232,12 @@ void setup()
   // initialize local devices
   // Initialize I2C communication
   Wire.begin();
-
+  // Initialize the Wi-Fi server
+  WiFiServer tcpServer(serverPort);
+  tcpServer.begin();
+  Serial.print("Server started on port ");
+  Serial.println(serverPort);
+  
   // Initialize HT16K33 LED matrix
   matrix.begin(LED_MATRIX_ADDR);
   matrix.setBrightness(10);
@@ -248,19 +292,40 @@ void setup()
 
 void loop()
 {
-  air_quality = readAndDisplayAirQuality();
-  sound_level = readAndDisplaySoundLevel();
-  if (isnan(air_quality) || isnan(sound_level)) // Check if any reads failed and exit early (to try again).
-  {
-    Serial.println(F("Failed to read from sensors!"));
-    return;
+  // Add code to handle incoming TCP client connections and receive data
+  WiFiClient wclient = tcpServer.available();
+
+  if (wclient) {
+    Serial.println("Wifi-Client connected");
+    
+    while (wclient.connected()) {
+      if (wclient.available()) {
+        int result = wclient.parseInt();
+        Serial.print("Received result: ");
+        Serial.println(result);
+        air_quality = readAndDisplayAirQuality();
+        sound_level = readAndDisplaySoundLevel();
+        if (isnan(air_quality) || isnan(sound_level)) // Check if any reads failed and exit early (to try again).
+        {
+          Serial.println(F("Failed to read from sensors!"));
+          return;
+        }
+        //print out serial values and publish message if air quality is above 0 and smaller than 2000 and sound level is above 0 and smaller than 100
+        Serial.print(F("Air_quality: "));
+        Serial.print(air_quality);
+        Serial.print(F(" Sound_level: "));
+        Serial.print(sound_level);
+        Serial.print(" dB");
+        /* processBluetoothData(); // Process incoming Bluetooth data */
+        publishMessage(air_quality, sound_level, result);
+      }
+    }
+
+    wclient.stop();
+    Serial.println("WifiClient disconnected");
   }
-  Serial.print(F("Air_quality: "));
-  Serial.print(air_quality);
-  Serial.print(F(" Sound_level: "));
-  Serial.print(sound_level);
-  /* processBluetoothData(); // Process incoming Bluetooth data */
-  publishMessage();
+  
+    
   client.loop();
   delay(2000);
 }
